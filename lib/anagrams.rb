3248d22d1565244eeadcdde069fdd1152342c558
Jared Clemmensen <jaredclemmensen@gmail.com>
2018-02-09 09:41:09 -0800

    pass case insensitivity

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -4,8 +4,8 @@ class Anagrams
   attr_accessor(:word1, :word2)
 
   def initialize(word1, word2)
-    @word1 = word1
-    @word2 = word2
+    @word1 = word1.downcase
+    @word2 = word2.downcase
   end
 
   def is_anagram?
